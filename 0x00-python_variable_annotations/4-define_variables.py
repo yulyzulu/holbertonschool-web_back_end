8f47e2013b487b67294f37a38af845c3d57040df
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-15 23:33:10 +0000

    Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Define variables"""
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
