5cb93eafe1782f5a66c78c7ec9194d3d22e2f8e4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-19 23:24:32 +0000

    Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 0-main """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Basic class"""
+
+    def __init__(self):
+        """Constructor """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign values to a dictionary"""
+        if (key is not None or item is not None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return cache data"""
+        if (key not in self.cache_data or key is None):
+            return None
+        else:
+            return self.cache_data[key]
