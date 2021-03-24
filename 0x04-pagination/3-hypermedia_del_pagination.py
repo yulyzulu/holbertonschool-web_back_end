97738b3fc95b5fc99d42694a8d184080150c86a8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-24 19:17:37 +0000

    Deletion-resilient hypermedia pagination

--- 0x04-pagination/3-hypermedia_del_pagination.py
+++ 0x04-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """ """
+        """Hypermedia pagination """
         indexed = self.indexed_dataset()
         indexed_data = len(indexed)
         assert index >= 0 and index < indexed_data
