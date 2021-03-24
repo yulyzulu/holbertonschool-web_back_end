e0cbb0d5caa021e09c26bc606cdbaba4958faedd
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-24 03:07:06 +0000

    Simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """The function return a tuple of size two containing a start index and
+           an end index corresponding to the range of indexes to return in a
+           list for those particular pagination parameters."""
+        return (page * page_size - page_size, page * page_size)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Method that return dataset"""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        page1, page_size1 = self.index_range(page, page_size)
+        dataset = self.dataset()
+        if page < page1 and page_size > page_size1:
+            return []
+        else:
+            return dataset[page1: page_size1]
