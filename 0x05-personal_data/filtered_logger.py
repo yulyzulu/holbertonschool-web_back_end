c793ce0eb03e78babe4b24604ff9d152e0d1ff4e
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-29 19:52:45 +0000

    Regex-ing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -8,10 +8,10 @@ import re
 def filter_datum(fields: List, redaction: str,
                  message: str, separator: str):
     """ Function that return the log message obfuscated"""
-    txt = ""
-    for x in message:
-        txt += x
+    #txt = ""
+    #for x in message:
+    #    txt += x
     for i in range(len(fields)):
-        val = re.findall(re.escape(fields[i] + "=")+"(.*?)"+re.escape(separator), txt)
-        x = re.sub(val[0], redaction, txt)
-    return x
+        val = re.findall(re.escape(fields[i] + "=")+"(.*?)"+re.escape(separator), message)
+        message = re.sub(val[0], redaction, message)
+    return message
