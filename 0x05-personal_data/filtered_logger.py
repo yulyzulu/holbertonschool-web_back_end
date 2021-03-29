43956e6e9b75764cfe4bb01d926bf073070dfa47
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-29 20:02:10 +0000

    Regex-ing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,11 +5,12 @@ Regex-ing file
 from typing import List
 import re
 
+
 def filter_datum(fields: List, redaction: str,
                  message: str, separator: str):
     """ Function that return the log message obfuscated"""
-    for i in range(len(fields)):
-        val = re.findall(re.escape(fields[i] + "=")+"(.*?)"+re.escape(separator),
+    for i in fields:
+        val = re.findall(re.escape(i + "=")+"(.*?)"+re.escape(separator),
                          message)
         message = re.sub(val[0], redaction, message)
     return message
