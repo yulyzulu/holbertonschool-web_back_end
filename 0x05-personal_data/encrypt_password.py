5d8b7d7d9db454f43bda92107e18755f751a6541
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-30 19:24:58 +0000

    Encrypting passwords

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,5 +8,5 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Function that expects one string argument name password and returns
         a salted, hashed password, which is a byte string."""
-    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
