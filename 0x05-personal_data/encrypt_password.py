ebe1f23e39c3376cb9521f9b657c3890d7dbf656
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-30 19:20:48 +0000

    Encrypting passwords

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """Function that expects one string argument name password and returns
+        a salted, hashed password, which is a byte string."""
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+    return hashed
