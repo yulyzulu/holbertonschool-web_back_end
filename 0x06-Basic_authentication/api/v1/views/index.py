53343d0c7b3064740f7e0318341d791cd090c782
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-03-30 21:46:35 +0000

    Error handler: Unauthorized

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ Executed 401 errror handler"""
+    abort(401)
