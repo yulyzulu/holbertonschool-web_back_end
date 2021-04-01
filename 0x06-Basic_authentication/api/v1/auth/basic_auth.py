23441bd04dbe316a1cafa615f14df7542401ca2a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-01 03:20:42 +0000

    Basic- user object

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """ Auth class"""
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -50,3 +51,19 @@ class BasicAuth(Auth):
             return decoded_header[0], decoded_header[1]
         else:
             return None, None
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """Method that returns the User instance based on his email
+           and password"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
