9f9eb6424a5cb7f750ba0b8e508c29e8b751ea79
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-01 02:49:24 +0000

    Basic-Base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """ Auth class"""
-from  api.v1.auth.auth import Auth
+from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """ Basic Auth class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
-        """ Method that returns the Base64 parth of the Authorization header"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Method that returns the Base64 parth of the Authorization
+            header"""
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
@@ -15,4 +18,18 @@ class BasicAuth(Auth):
         if authorization_header.split()[0] == "Basic":
             return authorization_header.split()[1]
         else:
+            return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Method that returns the decoded value of a Base64"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            message_bytes = base64.b64decode(base64_authorization_header)
+            message = message_bytes.decode('utf-8')
+            return message
+        except Exception:
             return None
