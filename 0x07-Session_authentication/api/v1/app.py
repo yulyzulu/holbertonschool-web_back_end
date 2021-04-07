81f0bab29ac02ed3d2489987c3c081eebf39e84a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-07 20:07:23 +0000

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -54,9 +54,9 @@ def before_request():
     list_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                   '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth:
-        return None
+        return
     if not auth.require_auth(request.path, list_paths):
-        return None
+        return
     if auth.authorization_header(request) is None\
        and auth.session_cookie(request) is None:
         abort(401)
