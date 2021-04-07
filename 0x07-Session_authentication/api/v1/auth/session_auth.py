5b3ee6cfdb9e31713709c9c3b85bd041da69981d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-07 02:49:29 +0000

    User ID for session ID

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,7 @@ class SessionAuth(Auth):
         """ Method that creates a Session ID for a user_id"""
         if user_id is None or type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
