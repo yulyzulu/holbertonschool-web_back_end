142989df40cffff7fc0c02773557df0936a504d4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-07 00:20:21 +0000

    Empty session

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if os.getenv('AUTH_TYPE') == 'auth':
 if os.getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+if os.getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 list_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Session Auth class"""
+from api.v1.auth.auth import Auth
+import base64
+from typing import TypeVar
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Session Auth class"""
+    pass
