a6cffb4dadf0764e35b6136076ccfde78ff2c964
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-07 03:19:40 +0000

    Session

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -24,8 +24,8 @@ if os.getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
-list_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
-              '/api/v1/auth_session/login/']
+#list_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+#              '/api/v1/auth_session/login/']
 
 
 @app.errorhandler(404)
@@ -50,12 +50,15 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """ Before request method"""
+    list_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+              '/api/v1/auth_session/login/']
     request.current_user = auth.current_user(request)
     if not auth:
         return None
     if not auth.require_auth(request.path, list_paths):
         return None
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None\
+       and auth.session_cookie(request) is None:
         abort(401)
     if not auth.authorization_header(request):
         abort(401)
