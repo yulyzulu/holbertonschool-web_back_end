9401e08c150d15e03126dbee196dfe6be5dd7d1e
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-11 17:25:27 +0000

    User model

---
+++ 0x08-user_authentication_service/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" User model """
+
+from sqlalchemy import Column, Integer, String
+
+class User():
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
