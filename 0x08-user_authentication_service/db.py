417c926607e16ae655440345d01b0a291e6d8b58
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-11 20:12:20 +0000

    Create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Database file
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from user import Base, User
+
+
+class DB:
+    """DB class"""
+
+    def __init__(self):
+        """ Constructor method"""
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """Private property and hence should NEVER be used from outside
+           the DB class"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> object:
+        """Add user method require email and hashed_password as
+           argumetns and return a User object"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        return new_user
