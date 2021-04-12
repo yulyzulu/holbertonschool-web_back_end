14d2e343e9f7ace63bf1a771dbccfb0fa59bd1de
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-12 16:18:50 +0000

    Change conditional

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -48,7 +48,7 @@ class DB:
             if i not in keywords:
                 raise InvalidRequestError
         data = self._session.query(User).filter_by(**kwargs).first()
-        if not data:
+        if data is None:
             raise NoResultFound
         return data
 
