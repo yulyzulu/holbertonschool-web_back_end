fdb05c69d3e7113727285c74678f71ca40bca50f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-12 13:34:12 +0000

    Create user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> object:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add user method require email and hashed_password as
            argumetns and return a User object"""
         new_user = User(email=email, hashed_password=hashed_password)
