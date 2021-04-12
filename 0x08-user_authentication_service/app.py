1f3060d07476901278fc567c644e59d405436cfa
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-12 19:30:13 +0000

    Basic flask app

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,13 +3,16 @@
 App file
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def welcome():
-    return flask.jsonify({"message": "Bienvenue"})
+def welcome() -> str:
+    return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
