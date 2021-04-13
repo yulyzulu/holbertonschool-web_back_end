abfcb2b8a392eabc12c8303ecf276f9e3059896d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-13 19:31:17 +0000

    Log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 App file
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -26,6 +26,19 @@ def users() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')    
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie('session_id', session_id)
+        return resp
+    else:
+        abort(401)
+        
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
