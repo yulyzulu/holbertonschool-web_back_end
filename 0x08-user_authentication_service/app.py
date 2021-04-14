74780db8c67ee388ce70e96fc3c11fa1f3550492
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-14 00:31:00 +0000

    User profile

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -54,6 +54,16 @@ def logout() -> str:
     else:
         abort(403)
 
+@app.route('/profile', strict_slashes=False)
+def profile() -> str:
+    """Function to find user """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
