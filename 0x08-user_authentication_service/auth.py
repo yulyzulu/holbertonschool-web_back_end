33b40663141132d1674356e6862867f7be9e26c4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-14 00:16:48 +0000

    Fix auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -63,6 +63,8 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """ Get user by session id"""
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
