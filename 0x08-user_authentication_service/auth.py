4175a2327396e51e16bf78a2ec781c450981d738
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-14 01:32:19 +0000

    Update password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -88,3 +88,13 @@ class Auth:
             return token
         except NoResultFound:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password method """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_pwd,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
