17dd08aa1d6d0f68d18a14dfa75beca3eeca33a0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-14 01:54:26 +0000

    Update password end-point

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -77,6 +77,21 @@ def get_reset_password_token() -> str:
     except Exception:
         abort(403)
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Function to update password"""
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+        token = AUTH.get_reset_password_token(email)
+        if token:
+            AUTH.update_password(reset_token, new_password)
+            msg = {"email": email, "message": "Password updated"}
+            return jsonify(msg), 200
+    except Exception:
+        abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
