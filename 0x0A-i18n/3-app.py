cc8e1ce00e92081958768462d38956c5b7d3794f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-22 20:54:27 +0000

    Parametrize templates

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -9,27 +9,26 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
+class Config:
+    """ Config class"""
     LANGUAGES = ["en", "fr"]
 
-app.Config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.Config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+app.config.from_object(Config)
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Get locale function"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-home_title = flash(_("Welcome to Holberton"))
-home_header = flask(_("Hello world!"))
 
 @app.route('/')
 def hello():
     """Greet function"""
-    return render_template('3-index.html', locale=get_locale(), home_title=home_title, home_header=home_header)
-
-
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
