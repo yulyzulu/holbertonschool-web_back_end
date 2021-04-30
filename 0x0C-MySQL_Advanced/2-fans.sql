bf1ae367cf1b5a9a71f5c4488031d81b7aaeea3d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-04-30 02:21:08 +0000

    Best band ever

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,2 +1,2 @@
 -- Script that ranks country origins of bands ordered by fans
-SELECT origin, fans as nb_fans FROM metal_bands ORDER BY fans;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
