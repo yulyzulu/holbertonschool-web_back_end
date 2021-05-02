d70e4ea00ed4a519deb386f7a0a11bc301463bc8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-02 22:15:18 +0000

    Safe Div

--- 0x0C-MySQL_Advanced/10-div.sql
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 -- Script that creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
 DELIMITER $$
-CREATE FUNCTION SaveDiv (a INT, B INT) RETURNS FLOAT
+CREATE FUNCTION SaveDiv (a INT, b INT) RETURNS FLOAT
 BEGIN
         DECLARE division FLOAT;
         IF (b = 0) THEN
