894a839a94a99d71254daffc7aa879700c33f728
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-05 01:26:11 +0000

    Top students

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Top students """
+
+def top_students(mongo_collection):
+    """Function that returns all students sorted by average score"""
+    average = mongo_collection.aggregate([
+        {$group:
+            {_id: "$_id",
+            name: "$name",
+            averageScore: {$avg: "$score"}}
+        },
+        {$sort: {averageScore:1}}
+    ])
+    return average
