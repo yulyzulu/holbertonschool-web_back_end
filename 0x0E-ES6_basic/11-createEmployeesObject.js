ca740974f907f5e17f52dbcc11171ad89deb2934
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-06 16:04:42 +0000

    Iterator

--- 0x0E-ES6_basic/11-createEmployeesObject.js
+++ 0x0E-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
   const obj = {
-    [`${departmentName}`]: employees,
+    [`${departmentName}`]: employees
   }
   return obj;
 }
