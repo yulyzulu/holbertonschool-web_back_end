f02abff52a12a0082dd57a16d370b432df6ed17c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-06 15:57:29 +0000

    Loop

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array1 = [];
   for (const idx of array) {
-    array1.push(appendString + idx)
+    array1.push(appendString + idx);
   }
 
   return array1
