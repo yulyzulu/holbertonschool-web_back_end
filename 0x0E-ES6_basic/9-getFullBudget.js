671f5a107d7d52036ab51f2e68444f6d1b2f31b8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-06 21:20:47 +0000

    Methods property

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(){return `$${this.income}`;},
-    getIncomeInEuros(){return `${this.income} euros`;},
+    getIncomeInDollars(income){return `$${income}`;},
+    getIncomeInEuros(income){return `${income} euros`;},
   };
 
   return fullBudget;
