49029917090b4d91b202297c46793ff2f7fa63b6
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 15:14:22 +0000

    Add else

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -2,7 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
-    reject(new Error('The fake API is not working currently'));
   });
 }
