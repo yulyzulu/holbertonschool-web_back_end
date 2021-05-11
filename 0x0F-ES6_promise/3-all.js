3cbee44616da59cae4f94a962c58f4cab95010c7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 22:01:09 +0000

    Handle multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,10 @@ import { uploadPhoto, createUser } from "./utils.js";
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(values => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
