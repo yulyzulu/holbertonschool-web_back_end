4c5d58b410150889dbb535f0de528ff01e538beb
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 20:58:24 +0000

    Handle multiple promises

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastName, fileName){
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  
+}
