eae6c36f9dda4f33ce5dedb9edef907c4436d8e4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 20:01:40 +0000

    Simple promise

--- 0x0F-ES6_promise/4-user-promise.js
+++ 0x0F-ES6_promise/4-user-promise.js
@@ -1,8 +1,6 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise ((resolve, reject) => {
-    resolve({
-      firstName,
-      lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
   });
 }
