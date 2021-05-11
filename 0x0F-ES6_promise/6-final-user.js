b28e469942ec9211637b7e4b8106d8a67a444429
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 23:59:36 +0000

    Handle multiple promises

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,7 +1,24 @@
 import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-  
+export default  async function handleProfileSignup(firstName, lastName, fileName) {
+  let promise1;
+  let promise2;
+  try {
+    const user = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = user;
+  } catch (error) {
+    promise1.status = 'rejected';
+    promise1.value = error.toString();
+  }
+  try {
+    const photo = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = photo;
+  } catch (error) {
+    promise2.status = 'rejected';
+    promise2.value = error.toString();
+  }
+  return [promise1, promise2];
 }
