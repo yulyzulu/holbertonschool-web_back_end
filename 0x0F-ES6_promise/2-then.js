37691229ff9bf5046a712dc649a247c795f27f51
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 19:08:43 +0000

    Catch me if you can

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,14 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({status: 200, body: 'success'});
-    } else {
-      reject(Error());
-    }
-  });
-
   promise
-    .then(res => {console.log('Got a response from the API')})
-    .catch(error => {console.log(error)})
-
+    .then({status: 200, body: 'success',})
+    .catch(Error())
+    .finally(console.log('Got a response from the API'))
 }
