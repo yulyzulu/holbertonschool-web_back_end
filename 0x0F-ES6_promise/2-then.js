0a385988efcd4ff1853046ef3bc14c2a514731df
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 15:47:05 +0000

    Catch

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,12 +2,13 @@ export default function handleResponseFromAPI(promise) {
   promise = new Promise((resolve, reject) => {
     if (promise) {
       resolve({status: 200, body: 'success'});
+    } else {
+      reject(Error());
     }
-    reject(new Error());
   });
 
   promise
-    .then(console.log('Got a response from the API'))
-    .catch(error => console.log(error))
+    .then(res => {console.log('Got a response from the API')})
+    .catch(error => {console.log(error)})
 
 }
