abf600f055b392ddc105d460fcc8a0122fb487df
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-11 22:19:42 +0000

    Throw error / try catch

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,7 +1,6 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    return numerador / denominador;
-  } catch {
+  if (denominator === 0) {
     throw 'cannot divide by 0';
   }
+  return numerator / denominator;
 }
