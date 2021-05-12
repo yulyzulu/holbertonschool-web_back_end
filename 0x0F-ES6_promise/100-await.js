5ccb880f72482cc1e38b06c731d58bfbb0e8761f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-12 00:17:09 +0000

    Await/Async

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+  let user;
+  let photo;
+
+  try {
+    user = await createUser();
+    photo = await uploadPhoto();
+  } catch (error) {
+    user = null;
+    photo = null;
+  }
+  return ({
+    photo,
+    user,
+  });
+}
