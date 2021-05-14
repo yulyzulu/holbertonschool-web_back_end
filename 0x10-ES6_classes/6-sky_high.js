1aaf0d38236e62868a829c03345bdd460f3e63fe
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-14 00:32:11 +0000

    Inheritance

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -5,6 +5,7 @@ export default class SkyHighBuilding extends Building {
     super(sqft);
     this._floors = floors;
   }
+
   get sqft() {
     return this._sqft;
   }
@@ -14,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this._floors} floors`);
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
