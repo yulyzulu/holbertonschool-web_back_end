b10ba70d1788aa4c75d8ce1ba49d2eb8179e45aa
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-18 20:01:58 +0000

    Combination

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  if (!Array.isArray(getListStudents) || typeof city !== 'string') {
+    return [];
+  }
+  const array = getListStudents.filter((item) => item.location === city).map((item, index) => {
+    const grades = newGrades.filter((grade) => grade.studentId === item.id);
+    if (!grades.length){
+      item.grade = 'N/A';
+    } else {
+      item.grade = grades[0].grade;
+    }
+    return item;
+  });
+  return array;
+}
