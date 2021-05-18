0f4b56cf1827849c0f42008fa58cf25e7d3f6465
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-18 14:59:51 +0000

    More mapping

---
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentIds(getList) {
+  const array = []
+  if (!Array.isArray(getList)) {
+    return array;
+  }
+
+  getList.map(item => array.push(item.id));
+  return array;
+}
