10b37011b929b7a9017dd0d073031e6abdc175fd
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-18 22:08:15 +0000

    Map data structure

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
   let list = new Map();
-  list['Apples'] = 10;
-  list['Tomatoes'] = 10;
-  list['Pasta'] = 1;
-  list['Rice'] = 1;
-  list['Banana'] = 5;
+  list.set('Apples', 10);
+  list.set('Tomatoes', 10);
+  list.set('Pasta', 1);
+  list.set('Rice', 1);
+  list.set('Banana', 5);
   return list;
 }
