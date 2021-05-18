4c804fd91fef0f1a35fe3329373dac900e7459b3
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-18 19:17:23 +0000

    Fix array

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!students.isArray && students.every(i => typeof i !== 'string')) throw new TypeError('Students must be an array of Strings');
+    if (!Array.isArray(students) && students.every(i => typeof i !== 'string')) throw new TypeError('Students must be an array of Strings');
 
     this._name = name;
     this._length = length;
