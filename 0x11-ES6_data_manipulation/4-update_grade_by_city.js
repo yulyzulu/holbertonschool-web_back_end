8441d1a53a69ae80c74835e0d7712caf4fd5eddf
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-19 13:30:09 +0000

    Combinate

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,6 @@
 export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
   const array = getListStudents.filter((item) => item.location === city).map((item) => {
-    const grades = newGrades.filter((grade) => grade.studentId === item.id);
+    const grades = newGrades.filter((grade) => grade.studentId === item.id, );
     if (!grades.length) {
       item.grade = 'N/A';
     } else {
