f78c61a5437ecd2ca61723391f03ec4114a547af
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-24 15:10:13 +0000

    Using Process stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', function() {
   var name = process.stdin.read();
   if (name !== null) {
