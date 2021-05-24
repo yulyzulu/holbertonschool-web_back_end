07a0a6a1e3822280f4cc90d8905dd81d3a87d700
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-24 19:51:24 +0000

    Reading a file synchronously with NodeJs

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    let students = fs.readFileSync(path).toString().split('\n');
+    students = students.slice(1, students.length - 1);
+    console.log(`Number of students: ${students.length}`);
+
+    const studentDescription = {};
+    for (const i of students) {
+      const student = i.split(',');
+      console.log(student);
+      if (!studentDescription[student[3]]) {
+        studentDescription[student[3]] = [];
+      }
+      studentDescription[student[3]].push(student[0]);
+    }
+    for (const item in studentDescription) {
+      if (item) {
+        console.log(`Number of students in ${item}: ${studentDescription[item].length}. List: ${studentDescription[item].join(', ')}`);
+      }
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
