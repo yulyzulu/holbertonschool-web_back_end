34e66b4abeba023ab1593ca9dfb687dd8b19a6a4
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-24 21:48:15 +0000

    Stdin

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
 process.stdin.resume();
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', function() {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
