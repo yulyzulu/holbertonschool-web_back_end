bbb582c796ec96c9384906d027dba59355fddbe7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-24 22:26:41 +0000

    Create a small HTTP server using Node's HTTP module

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Contetn-Type', 'text/plain');
   res.end('Hello Holberton School!');
 }).listen(1245);
 
