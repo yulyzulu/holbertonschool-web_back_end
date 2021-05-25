5c38101fe241e49f3ede0bb345248df2c23488be
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-25 00:58:17 +0000

    Http express

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const countStudents = require('./3-read_file_async');
+const args = process.argv.slice(2);
+
+const database = args[0];
+
+const app = express();
+
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const text = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(database);
+    res.send(`${text}${students.join('\n')}`);
+  } catch (err) {
+    res.send(`${text}${err.message}`);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
+
+module.exports = app;
