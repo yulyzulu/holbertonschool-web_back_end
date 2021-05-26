d2b779ec4736f8386a8816f30c658858458a2e18
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-26 19:09:53 +0000

    Use mocha test

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
-const sum = require('./0-calcul.js');
+const calculateNumber = require('./0-calcul.js');
 
-describe('Returns the expected sum', () => {
-  it('Should return correct sum', () => {
-    assert.equal(sum.calculateNumber(3, 4), 7);
+describe('Returns the expected sum', function() {
+  it('Should return correct sum', function() {
+    assert.equal(calculateNumber(3, 4), 7);
   });
-  it('Should return rounded sum', () => {
-    assert.equal(sum.calculateNumber(1, 3.7), 5);
+  it('Should return rounded sum', function() {
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
-  it('Should return rounded sum', () => {
-    assert.equal(sum.calculateNumber(1.2, 3.7), 5);
+  it('Should return rounded sum', function() {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('Should return rounded sum', () => {
-    assert.equal(sum.calculateNumber(1.5, 3.7), 6);
+  it('Should return rounded sum', function() {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('Should return rounded sum', () => {
-    assert.equal(sum.calculateNumber(-1.2, -3.8), -5);
+  it('Should return rounded sum', function() {
+    assert.equal(calculateNumber(-1.2, -3.8), -5);
   });
 });
