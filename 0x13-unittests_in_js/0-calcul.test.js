2631dc43886115b366023bac2e68ded82dd415df
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-05-26 18:53:22 +0000

    Mocha

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
 const sum = require('./0-calcul.js');
 
-describe('Returns the expected sum', function(){
-  it('Should return correct sum', function() {
+describe('Returns the expected sum', () => {
+  it('Should return correct sum', () => {
     assert.equal(sum.calculateNumber(3, 4), 7);
   });
-  it('Should return rounded sum', function() {
+  it('Should return rounded sum', () => {
     assert.equal(sum.calculateNumber(1, 3.7), 5);
   });
-  it('Should return rounded sum', function() {
+  it('Should return rounded sum', () => {
     assert.equal(sum.calculateNumber(1.2, 3.7), 5);
   });
-  it('Should return rounded sum', function() {
+  it('Should return rounded sum', () => {
     assert.equal(sum.calculateNumber(1.5, 3.7), 6);
   });
 });
